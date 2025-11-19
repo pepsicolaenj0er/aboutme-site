7bf21128c5df8a76d99f0429a78748ed4081e791
Maxim <maxim.lyan@bk.ru>
2025-11-19 19:34:55 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import './App.css'
+
+export default function App() {
+  return (
+  <div className='header'>
+  <img src="/zxczxcursed.mp4" alt="roflanebalo" className='lico'/>
+  <h1>Maxim Lyan</h1>
+  <p>Web Solutions Developer</p>
+  <div className="biography"> 
+  <h2>ABOUT ME:</h2>
+  <p>I create modern, reliable, and user-friendly digital products.
+  I focus on detail, clarity, 
+  and delivering results that are not only functional but also comfortable for users and easy to 
+  maintain and scale in the future.</p>
+  <div className='mail'>
+  <a href="mailto:maximka.lyan@bk.ru" className='mail-btn'>My Email</a>
+  <img src="telegram.svg" href="https://t.me/eternalharvest" alt="tgicon"className='tgicon'/>
+  <img src="github-color.svg" href="https://github.com/pepsicolaenj0er"alt="giticon"className='giticon' />
+  
+  </div>
+  <div className='Project-Involvement'>
+  <h3>How I Work</h3>
+  <p>I can join a project at any stage — 
+  from the initial concept and structure to 
+  refinement, optimization, and ongoing support.</p>
+  <div className='Services'>
+  <h3>What I Do:</h3>
+  <ul className="Frontend">
+<h2>Frontend</h2>
+<li>Build responsive and interactive user interfaces</li>
+<li>Optimize performance and loading speed</li>
+<li>Implement smooth UX interactions and visual elements</li>
+  </ul>
+  <ul className='Backend'>
+<h2>Backend</h2>
+<li>Develop server logic and manage databases</li>
+<li>Work with APIs and integrate third-party services</li>
+<li>Ensure security, stability, and data consistency</li>
+ </ul>
+  <ul className="Technologies">
+<h2>Technologies</h2>
+<li>Use modern development tools and frameworks</li>
+<li>Version control, testing, debugging, and code optimization</li>
+<li>Automate tasks and improve development workflow</li>
+  </ul>
+  <ul className="Other">
+<h2>Other</h2>
+<li>Conduct code reviews and fix bugs</li>
+<li>Improve architecture and project logic</li>
+<li>Provide guidance and support throughout the project lifecycle</li>
+  </ul>
+  <div className='Footer'>
+<p>all rights reserved</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+ 
+  );
+}
