767d8c568b16d06fb9a4df48eabcb79a9ab17534
Maxim <maxim.lyan@bk.ru>
2025-11-19 20:21:12 +0500

    fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -14,37 +14,44 @@ export default function App() {
   maintain and scale in the future.</p>
   <div className='mail'>
   <a href="mailto:maximka.lyan@bk.ru" className='mail-btn'>My Email</a>
-  <img src="telegram.svg" href="https://t.me/eternalharvest" alt="tgicon"className='tgicon'/>
-  <img src="github-color.svg" href="https://github.com/pepsicolaenj0er"alt="giticon"className='giticon' />
-  
+  </div>
+  <div className='tgicon'>
+  <a href="https://t.me/eternalharvest">
+  <img src="telegram.svg" alt="tglogo"/>
+  </a>
+  </div>
+  <div className='giticon'>
+  <a href="https://github.com/pepsicolaenj0er">
+  <img src="github-color.svg" alt="gitlogo" width="30" height="30"/>
+  </a>
   </div>
   <div className='Project-Involvement'>
-  <h3>How I Work</h3>
+  <h2>HOW I WORK</h2>
   <p>I can join a project at any stage — 
   from the initial concept and structure to 
   refinement, optimization, and ongoing support.</p>
   <div className='Services'>
   <h3>What I Do:</h3>
   <ul className="Frontend">
-<h2>Frontend</h2>
+<h4>Frontend</h4>
 <li>Build responsive and interactive user interfaces</li>
 <li>Optimize performance and loading speed</li>
 <li>Implement smooth UX interactions and visual elements</li>
   </ul>
   <ul className='Backend'>
-<h2>Backend</h2>
+<h4>Backend</h4>
 <li>Develop server logic and manage databases</li>
 <li>Work with APIs and integrate third-party services</li>
 <li>Ensure security, stability, and data consistency</li>
  </ul>
   <ul className="Technologies">
-<h2>Technologies</h2>
+<h4>Technologies</h4>
 <li>Use modern development tools and frameworks</li>
 <li>Version control, testing, debugging, and code optimization</li>
 <li>Automate tasks and improve development workflow</li>
   </ul>
   <ul className="Other">
-<h2>Other</h2>
+<h4>Other</h4>
 <li>Conduct code reviews and fix bugs</li>
 <li>Improve architecture and project logic</li>
 <li>Provide guidance and support throughout the project lifecycle</li>
